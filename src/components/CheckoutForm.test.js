3aa604ec0433f3364b67178e422b24ec65ae2fb0
Durrell Jules <dujules23@gmail.com>
2021-06-11 21:26:17 -0400

    created test for render and first half of test to make sure the form is functional. Form can take in data and it does submit. Will check for render of data and success message momentarily

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,48 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event"
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    // Arrange:
+    render(<CheckoutForm />);
 
-test("form shows success message on submit with form details", () => {});
+    // Act:
+    const header = screen.getByText(/checkout form/i);
+
+    // Arrange:
+    expect(header).toBeInTheDocument();
+    expect(header).toBeTruthy();
+
+
+});
+
+test("form shows success message on submit with form details", () => {
+    // Arrange:
+    render(<CheckoutForm />);
+
+    const firstName = screen.getByLabelText(/first name/i);
+    const lastName = screen.getByLabelText(/last name/i);
+    const address = screen.getByLabelText(/address/i);
+    const city = screen.getByLabelText(/city/i);
+    const state = screen.getByLabelText(/state/i);
+    const zip = screen.getByLabelText(/zip/i);
+
+    const submitButton = screen.getByRole("button", { name: /checkout/i})
+
+    // Act:
+    userEvent.type(firstName, "Jacob");
+    userEvent.type(lastName, "Smith");
+    userEvent.type(address, "333 See You There Lane");
+    userEvent.type(city, "Wakanda");
+    userEvent.type(state, "WA");
+    userEvent.type(zip, "40392");
+
+    userEvent.click(submitButton);
+
+
+    // Assert:
+
+});
